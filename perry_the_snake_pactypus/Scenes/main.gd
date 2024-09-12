9de4b98b2ac2042660eecdd97f05db181a418cfe
Christopher Yee <yeec26@up.edu>
2024-09-11 22:03:10 -0700

    PELLETS WORK!
    Co-Authored-By: HenrySchiffUP <112667731+HenrySchiffUP@users.noreply.github.com>

--- perry_the_snake_pactypus/Scenes/main.gd
+++ perry_the_snake_pactypus/Scenes/main.gd
@@ -3,13 +3,13 @@ extends Node2D
 # idea: disincentivize the player from staying still by decreasing thier length/score after a couple seconds
 
 # the max number of segments the player can have at once (till they grow)
-var body_length: int = 25
-
+var body_length: float = 1.0
 
 var body_segment_scene: PackedScene = load("res://Scenes/BodySegment.tscn")
 
 @onready var player: PlayerHead = $PlayerHead
 @onready var body_segments_list: Node2D = $BodySegmentsList
+@onready var pellets_list: Node = $Pellets
 
 func _on_player_new_body_segment():
 	
@@ -23,3 +23,11 @@ func _on_player_new_body_segment():
 	if (body_segments_list.get_child_count() > body_length):
 		var last_body_segment: Area2D = body_segments_list.get_child(0)
 		last_body_segment.queue_free()
+
+func _ready():
+	for pellet in pellets_list.get_children():
+		pellet.pellet_eaten.connect(on_pellet_eaten)
+
+func on_pellet_eaten(should_allow_eating_ghosts: bool):
+	body_length += .2
+	
