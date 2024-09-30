0d20580343ed64acff2089083fcc57dabad6da0c
starrnakamitsu <nakamits26@up.edu>
2024-09-30 15:20:21 -0700

    doof teleports

--- perry_the_snake_pactypus/Scenes/doof.gd
+++ perry_the_snake_pactypus/Scenes/doof.gd
@@ -61,13 +61,9 @@ func _on_navigation_agent_2d_navigation_finished() -> void:
 func body_collision() -> void:
 	print("hadlks")
 	var pos
-	$CollisionTimer.start() # looks for a random position for 5 seconds
+	$CollisionTimer.start() # goes to a random position for 3 seconds
 	$Timer.set_paused(true) # pauses timer that looks for player
-	match randi_range(0, 3):
-		0: pos = Vector2(0,0)
-		1: pos = Vector2(0, 1000)
-		2: pos = Vector2(1000, 0)
-		3: pos = Vector2(1000, 1000)
+	pos = Vector2(randi_range(50, 990), randi_range(50,990))
 	$NavigationAgent2D.target_position = pos
 	
 # after doof hits body and can chase player
--- perry_the_snake_pactypus/Scenes/warp_zone_pair.gd
+++ perry_the_snake_pactypus/Scenes/warp_zone_pair.gd
@@ -6,15 +6,23 @@ class_name WarpZonePair
 @onready var zone_2: Area2D = $Zone2
 
 func _on_zone_1_body_entered(body):
-	if (body.movement_direction != Vector2.LEFT):
-		return
-	
-	body.global_position = zone_2.global_position - Vector2(40, 0)
-	body.get_parent().create_new_chain()
+	# checks for perry
+	if (body.is_in_group("player_head")):
+		if (body.movement_direction != Vector2.LEFT):
+			return
+		body.global_position = zone_2.global_position - Vector2(40, 0)
+		body.get_parent().create_new_chain()
+	# doofs
+	else:
+		body.global_position = zone_2.global_position - Vector2(40, 0)
 
 func _on_zone_2_body_entered(body):
-	if (body.movement_direction != Vector2.RIGHT):
-		return
-	
-	body.global_position = zone_1.global_position + Vector2(40, 0)
-	body.get_parent().create_new_chain()
+	# checks for perry
+	if (body.is_in_group("player_head")):
+		if (body.movement_direction != Vector2.RIGHT):
+			return
+		body.global_position = zone_1.global_position + Vector2(40, 0)
+		body.get_parent().create_new_chain()
+	# doofs
+	else:
+		body.global_position = zone_1.global_position + Vector2(40, 0)
